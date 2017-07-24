e6e6a2f78ad338d9a4b4099a0c0fc26158647f11
Paul Ewing <pcewing00@gmail.com>
2017-07-23 21:42:44 -0700

    Added the turret and mouse aim controls
    - Updated placeholder sprites
    - Added an enemy unit
    - Added basic projectiles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     public float speed;
 
-    private Rigidbody2D rb2d;
+    private GameObject ship;
+    private GameObject turret;
+
+    private Rigidbody2D shipRigidBody;
+    private Rigidbody2D turretRigidBody;
+
+    private GameObject projectilePrefab;
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        ship = GameObject.Find("Player/Ship");
+        turret = GameObject.Find("Player/Turret");
+
+        shipRigidBody = ship.GetComponent<Rigidbody2D>();
+        turretRigidBody = turret.GetComponent<Rigidbody2D>();
+
+        projectilePrefab = Resources.Load("Projectile") as GameObject;
     }
 
-    void FixedUpdate()
+    void Update()
+    {
+        MovePlayer();
+        SyncTurretAndShipPositions();
+        AimTowardsMouse();
+        HandleFiring();
+    }
+
+    void MovePlayer()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
+        shipRigidBody.AddForce(movement * speed);
+    }
+
+    void SyncTurretAndShipPositions()
+    {
+        turret.transform.position = ship.transform.position;
+    }
+
+    void AimTowardsMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 position = turret.transform.position;
+
+        Vector3 relative = new Vector3(mousePosition.x - position.x, mousePosition.y - position.y);
+        float angle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg;
+
+        turretRigidBody.rotation = angle;
+    }
+
+    void HandleFiring()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(projectilePrefab) as GameObject;
+            projectile.transform.position = turret.transform.position + turret.transform.forward * 10;
+
+            var projectileRigidBody = projectile.GetComponent<Rigidbody2D>();
+            projectileRigidBody.velocity = turret.transform.right * 40;
+        }
     }
 }
